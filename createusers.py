6c3720dff86b1f0a6c07516dee9d127f5e5f2cc8
administrator <bdaloukas@gmail.com>
2016-09-27 09:49:41 +0300

    Fix: createusers.py

--- createusers.py
+++ createusers.py
@@ -37,7 +37,7 @@ def append_user_to_group( username, groupname, group):
 #Φτιάχνω τους χρήστες
 def create_users(groupname, computers, tmimata):
     usernames = create_usernames( computers, tmimata)
-    print usernames
+    #print usernames
 
 	#Αρχικά φτιάχνω το group με όνομα groupname
     try:
@@ -57,16 +57,15 @@ def create_users(groupname, computers, tmimata):
     except:
         raise GroupDatabaseException(_("Failed to get the group list"))    
 
-	#Στη συνέχεια φτιάχνω τους χρήστες
+    #Στη συνέχεια φτιάχνω τους χρήστες
     users = []
     for p in pwd.getpwall():
         users.append( p[0])
     for username in usernames:
         if( username not in users):
-            print "createuser " + username
             createUser( username, username, username)
 
-	#Βάζω τους χρήστες στο κατάλληλο group
+    #Βάζω τους χρήστες στο κατάλληλο group
     usernames.append( "administrator")
     for group in groups:
         if group[0] == groupname:
@@ -78,11 +77,12 @@ def create_users(groupname, computers, tmimata):
         for tmima in tmimata:
             if group[0] == tmima:
                 #Βρήκα το group του τμήματος
+                append_user_to_group( "administrator", tmima, group)
                 for username in usernames:
                     if username[ -2:] == tmima:
                         append_user_to_group( username, tmima, group)
 
 
-computers = ['a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7']
-tmimata = ['', 'a1', 'a2', 'b1']
+computers = ['a1', 'a2', 'a3', 'a4', 'a5', 'b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'c1', 'c2', 'c3', 'c4', 'c5']
+tmimata = ['', 'a1', 'a2', 'a3', 'a4', 'b1', 'b2', 'b3', 'b4', 'c1', 'c1', 'c2', 'c3', 'c4', 'a1b', 'a2b', 'a3b', 'a4b', 'b1b', 'b2b', 'b3b', 'b4b', 'c1b', 'c1b', 'c2b', 'c3b', 'c4b']
 create_users( "erg", computers, tmimata)
